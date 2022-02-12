612767a6658e21d78808b5e948cdb0a46319608f
invi1998 <2267661826@qq.com>
2022-02-12 15:12:01 +0800

    有权图的稀疏图表示（邻接表表示）

--- 图论/有权图/main.cpp
+++ 图论/有权图/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include "DenseGraph.h"
 #include "ReadGraph.h"
+#include "SparseGraph.h"
 
 using namespace std;
 
@@ -24,6 +25,20 @@ int main()
   // NULL    0.32    NULL    NULL    0.35    NULL    NULL    0.28
   // 0.58    NULL    0.40    0.52    0.93    NULL    NULL    NULL
   // 0.16    0.19    0.34    NULL    0.37    0.28    NULL    NULL
+  std::cout << std::endl;
+
+  std::cout << "----------------------------------------------------------------------------------" << std::endl;
+  SparseGraph<double> g2 = SparseGraph<double>(V, false);
+  ReadGraph<SparseGraph<double>, double> read2(g2, filename1);
+  g2.show();
+  // vertex 0 :      ( to: 7, wt: 0.16 )     ( to: 4, wt: 0.38 )     ( to: 2, wt: 0.26 )     ( to: 6, wt: 0.58 )
+  // vertex 1 :      ( to: 5, wt: 0.32 )     ( to: 7, wt: 0.19 )     ( to: 2, wt: 0.36 )     ( to: 3, wt: 0.29 )
+  // vertex 2 :      ( to: 3, wt: 0.17 )     ( to: 0, wt: 0.26 )     ( to: 1, wt: 0.36 )     ( to: 7, wt: 0.34 )     ( to: 6, wt: 0.40 )
+  // vertex 3 :      ( to: 2, wt: 0.17 )     ( to: 1, wt: 0.29 )     ( to: 6, wt: 0.52 )
+  // vertex 4 :      ( to: 5, wt: 0.35 )     ( to: 7, wt: 0.37 )     ( to: 0, wt: 0.38 )     ( to: 6, wt: 0.93 )
+  // vertex 5 :      ( to: 4, wt: 0.35 )     ( to: 7, wt: 0.28 )     ( to: 1, wt: 0.32 )
+  // vertex 6 :      ( to: 2, wt: 0.40 )     ( to: 3, wt: 0.52 )     ( to: 0, wt: 0.58 )     ( to: 4, wt: 0.93 )
+  // vertex 7 :      ( to: 4, wt: 0.37 )     ( to: 5, wt: 0.28 )     ( to: 0, wt: 0.16 )     ( to: 1, wt: 0.19 )     ( to: 2, wt: 0.34 )
   std::cout << std::endl;
 
   return 0;
