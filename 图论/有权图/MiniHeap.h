8b1d85f7f3c023e18075737c4860857d3a2cf0af
invi1998 <2267661826@qq.com>
2022-02-14 09:38:20 +0800

    先学完最小堆再回来写

---
+++ 图论/有权图/MiniHeap.h
@@ -0,0 +1,23 @@
+//
+// 最小生成树算法辅助队列（最小堆）
+// 先学完最小堆再回来写
+//
+
+#ifndef MINIHEAP_H
+#define MINIHEAP_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename Item>
+class MiniHeap
+{
+
+private:
+  Item *data;
+  int count;
+  int capatity;
+};
+
+#endif
