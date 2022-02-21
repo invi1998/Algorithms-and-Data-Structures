497112a477d698d503e4d1f8e5fa8479646177d4
invi1998 <2267661826@qq.com>
2022-02-21 19:38:23 +0800

    shift down（从最大堆中取出一个元素）

--- 堆排序/heap/main.cpp
+++ 堆排序/heap/main.cpp
@@ -14,5 +14,16 @@ int main()
 
   mh.testPrint();
 
+  std::cout << std::endl
+            << "堆中数据从大到小排列：" << std::endl;
+
+  // 依据最大堆的方式，一步一步的将这些堆中的元素取出来
+  while (!mh.isEmpty())
+  {
+    std::cout << mh.extractMax() << " ";
+  }
+
+  std::cout << std::endl;
+
   return 0;
 }
