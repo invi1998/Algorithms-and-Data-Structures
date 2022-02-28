cc3954bc5ab4a7283983a098dbaf2f809cf01df4
invi1998 <2267661826@qq.com>
2022-02-28 11:02:30 +0800

    路径压缩进一步优化

--- 并查集/UnionFind/UnionFind5.h
+++ 并查集/UnionFind/UnionFind5.h
@@ -39,17 +39,30 @@ namespace UF5
     int find(int p)
     {
       assert(p >= 0 && p < count);
-      // find操作就是不断的通过p索引追溯他的父亲节点，知道他的父亲节点等于自身
-      while (parent[p] != p)
-      {
-        // 将p这个节点指向父亲的这个指针，指向p这个节点它父亲的父亲
-        parent[p] = parent[parent[p]]; // 路径压缩过程
+      // find操作就是不断的通过p索引追溯他的父亲节点，直到他的父亲节点等于自身
+      // while(parent[p] != p)
+      //   {
+      //     // 将p这个节点指向父亲的这个指针，指向p这个节点它父亲的父亲
+      //     parent[p] = parent[parent[p]];  // 路径压缩过程
 
-        // 路径压缩完成之后，继续让p = parent[p]，继续进行下一轮的检查
-        p = parent[p];
+      //     // 路径压缩完成之后，继续让p = parent[p]，继续进行下一轮的检查
+      //     p = parent[p];
+      //   }
+
+      // return p;
+
+      // 进一步优化压缩路径
+
+      // 如果当前的这个p不是一个根节点
+      if (p != parent[p])
+      {
+        // 要注意这里的这个递归逻辑，这里find(parent[p])一定会指向p的根节点
+        // 然后将parent[p]，p的父亲节点指向这个根节点
+        // 那么在这个递归过程中所遍历到的每一个节点，他们的parent都会指向根节点
+        parent[p] = find(parent[p]);
       }
 
-      return p;
+      return parent[p];
     }
 
     bool isConnected(int p, int q)
